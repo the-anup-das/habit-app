e535bd1d3331ea16fc2ae8c3a39a90d07c549ced
theanupllm <theanupdas@protonmail.com>
2026-08-05 21:51:05 +0530

    fix(mobile): patch react-native and libsodium for hermesc compatibility on android release build

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = (api) => {
   api.cache(true);
   return {
-    presets: [require.resolve("babel-preset-expo")],
+    presets: [['babel-preset-expo', { unstable_transformProfile: 'default' }]],
   };
 };
